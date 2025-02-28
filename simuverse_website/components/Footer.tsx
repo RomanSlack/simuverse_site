3edd5d9622579e612f134f655d3588ae931d3c07
Roman Slack <romanslack1@gmail.com>
2025-02-28 18:04:19 -0500

    Ah

--- simuverse_website/components/Footer.tsx
+++ simuverse_website/components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                 <a href="mailto:romanslack1@gmail.com" className="text-gray-300 hover:text-mars-orange transition-colors">
-                  contact@simuverse.ai
+                  romanslack1@gmail.com
                 </a>
               </li>
               <li className="flex items-center">
